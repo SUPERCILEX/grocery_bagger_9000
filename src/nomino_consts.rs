7629301981d34d116ed11d85889aa8d062d9b284
Alex Saveau <saveau.alexandre@gmail.com>
2022-05-04 13:16:14 -0700

    Fix colliders so they work on mirrored pieces
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/nomino_consts.rs
+++ src/nomino_consts.rs
@@ -26,7 +26,7 @@ pub static TETROMINO_STRAIGHT_COLLIDER: SyncLazy<Collider> = SyncLazy::new(|| {
     Collider::compound(vec![(
         Vec3::new(0., -0.5, 0.),
         Quat::IDENTITY,
-        Collider::cuboid(0.49, 1.99, 0.),
+        Collider::cuboid(0.49, 1.99, 0.1),
     )])
 });
 
@@ -46,7 +46,7 @@ pub static TETROMINO_SQUARE_COLLIDER: SyncLazy<Collider> = SyncLazy::new(|| {
     Collider::compound(vec![(
         Vec3::new(0.5, 0.5, 0.),
         Quat::IDENTITY,
-        Collider::cuboid(0.99, 0.99, 0.),
+        Collider::cuboid(0.99, 0.99, 0.1),
     )])
 });
 
@@ -68,11 +68,15 @@ pub static TETROMINO_T_PATH: SyncLazy<Path> = SyncLazy::new(|| {
 
 pub static TETROMINO_T_COLLIDER: SyncLazy<Collider> = SyncLazy::new(|| {
     Collider::compound(vec![
-        (Vec3::ZERO, Quat::IDENTITY, Collider::cuboid(0.49, 1.49, 0.)),
+        (
+            Vec3::ZERO,
+            Quat::IDENTITY,
+            Collider::cuboid(0.49, 1.49, 0.1),
+        ),
         (
             Vec3::new(0.99, 0., 0.),
             Quat::IDENTITY,
-            Collider::cuboid(0.5, 0.49, 0.),
+            Collider::cuboid(0.5, 0.49, 0.1),
         ),
     ])
 });
@@ -93,11 +97,15 @@ pub static TETROMINO_L_PATH: SyncLazy<Path> = SyncLazy::new(|| {
 
 pub static TETROMINO_L_COLLIDER: SyncLazy<Collider> = SyncLazy::new(|| {
     Collider::compound(vec![
-        (Vec3::ZERO, Quat::IDENTITY, Collider::cuboid(0.49, 1.49, 0.)),
+        (
+            Vec3::ZERO,
+            Quat::IDENTITY,
+            Collider::cuboid(0.49, 1.49, 0.1),
+        ),
         (
             Vec3::new(0.99, -1., 0.),
             Quat::IDENTITY,
-            Collider::cuboid(0.5, 0.49, 0.),
+            Collider::cuboid(0.5, 0.49, 0.1),
         ),
     ])
 });
@@ -119,14 +127,14 @@ pub static TETROMINO_SKEW_PATH: SyncLazy<Path> = SyncLazy::new(|| {
 });
 
 pub static TETROMINO_SKEW_COLLIDER: SyncLazy<Collider> = SyncLazy::new(|| {
-    let sub_bar = Collider::cuboid(0.49, 0.99, 0.);
+    let sub_bar = Collider::cuboid(0.49, 0.99, 0.1);
     Collider::compound(vec![
         (Vec3::new(0., -0.5, 0.), Quat::IDENTITY, sub_bar.clone()),
         (Vec3::new(1., 0.5, 0.), Quat::IDENTITY, sub_bar),
         (
             Vec3::new(0.5, 0., 0.),
             Quat::IDENTITY,
-            Collider::cuboid(0.1, 0.49, 0.),
+            Collider::cuboid(0.1, 0.49, 0.1),
         ),
     ])
 });
