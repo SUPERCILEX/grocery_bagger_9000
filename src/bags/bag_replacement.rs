1664cde28264bc8e44559cb5f9179242739f334b
Alex Saveau <saveau.alexandre@gmail.com>
2022-05-14 21:14:08 -0700

    Remove non-empty filled bags at the end of the level and don't spawn in a new one
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/bags/bag_replacement.rs
+++ src/bags/bag_replacement.rs
@@ -1,4 +1,4 @@
-use bevy::{math::const_vec3, prelude::*};
+use bevy::{math::const_vec3, prelude::*, transform::TransformSystem::TransformPropagate};
 use bevy_rapier3d::prelude::*;
 use bevy_tweening::TweenCompleted;
 use smallvec::SmallVec;
@@ -12,6 +12,7 @@ use crate::{
         BagSpawner, BAG_LID_COLLIDER_GROUP, RADIUS,
     },
     conveyor_belt,
+    conveyor_belt::BeltEmptyEvent,
     levels::CurrentLevel,
     nominos::{NominoMarker, PiecePlaced, NOMINO_COLLIDER_GROUP},
 };
@@ -21,15 +22,38 @@ pub struct BagReplacementPlugin;
 impl Plugin for BagReplacementPlugin {
     fn build(&self, app: &mut App) {
         app.add_event::<BagFilled>();
+        app.add_event::<RemoveFilledBag>();
+        app.add_event::<ReplaceFilledBag>();
 
         app.add_system_to_stage(CoreStage::PostUpdate, detect_filled_bags);
-        app.add_system(replace_full_bags);
+        app.add_system_to_stage(
+            CoreStage::PostUpdate,
+            replace_full_bags.after(detect_filled_bags),
+        );
+        app.add_system_to_stage(
+            CoreStage::PostUpdate,
+            remove_filled_bags
+                .after(replace_full_bags)
+                .after(TransformPropagate),
+        );
+        app.add_system_to_stage(
+            CoreStage::PostUpdate,
+            replace_filled_bags
+                .after(replace_full_bags)
+                .after(TransformPropagate),
+        );
         app.add_system_to_stage(CoreStage::PostUpdate, despawn_bags);
     }
 }
 
 #[derive(Deref)]
 struct BagFilled(Entity);
+
+#[derive(Deref)]
+struct RemoveFilledBag(Entity);
+
+#[derive(Deref)]
+struct ReplaceFilledBag(Entity);
 
 #[derive(Component, Deref, DerefMut)]
 pub struct BagPieces(pub SmallVec<[Entity; conveyor_belt::MAX_NUM_PIECES]>);
@@ -90,10 +114,42 @@ fn detect_filled_bags(
     }
 }
 
+#[derive(Debug, Default)]
+pub struct BagReplacementFsm {
+    belt_empty: bool,
+}
+
 fn replace_full_bags(
-    mut commands: Commands,
+    mut replacement_fsm: Local<BagReplacementFsm>,
     mut filled_events: EventReader<BagFilled>,
-    current_level: Res<CurrentLevel>,
+    mut belt_empty_events: EventReader<BeltEmptyEvent>,
+    mut piece_placements: EventReader<PiecePlaced>,
+    mut remove_events: EventWriter<RemoveFilledBag>,
+    mut replace_events: EventWriter<ReplaceFilledBag>,
+    bags: Query<Entity, With<BagMarker>>,
+) {
+    if belt_empty_events.iter().count() > 0 {
+        replacement_fsm.belt_empty = true;
+    }
+
+    for filled_bag in filled_events.iter() {
+        if !replacement_fsm.belt_empty {
+            remove_events.send(RemoveFilledBag(**filled_bag));
+            replace_events.send(ReplaceFilledBag(**filled_bag));
+        }
+    }
+
+    if piece_placements.iter().count() > 0 && replacement_fsm.belt_empty {
+        for bag in bags.iter() {
+            remove_events.send(RemoveFilledBag(bag));
+        }
+        *replacement_fsm = default();
+    }
+}
+
+fn remove_filled_bags(
+    mut commands: Commands,
+    mut remove_events: EventReader<RemoveFilledBag>,
     game_speed: Res<GameSpeed>,
     bag_positions: Query<&Transform, With<BagMarker>>,
     bag_pieces: Query<&BagPieces, With<BagMarker>>,
@@ -102,8 +158,40 @@ fn replace_full_bags(
         (With<NominoMarker>, Without<BagMarker>),
     >,
 ) {
-    for filled_bag in filled_events.iter() {
-        let current_bag_position = bag_positions.get(**filled_bag).unwrap();
+    for removed_bag in remove_events.iter() {
+        let current_bag_position = bag_positions.get(**removed_bag).unwrap();
+        let exit_bag_position = {
+            let mut p = *current_bag_position;
+            p.translation.y = -(RADIUS + 0.5);
+            p
+        };
+
+        commands.entity(**removed_bag).insert(animations::bag_exit(
+            *current_bag_position,
+            exit_bag_position,
+            &game_speed,
+        ));
+
+        // TODO remove after https://github.com/dimforge/bevy_rapier/issues/172
+        for piece in &**bag_pieces.get(**removed_bag).unwrap() {
+            commands.entity(**removed_bag).add_child(*piece);
+
+            let bag_global = current_bag_position.translation;
+            let (piece_global, mut piece_local) = piece_positions.get_mut(*piece).unwrap();
+            piece_local.translation = piece_global.translation - bag_global;
+        }
+    }
+}
+
+fn replace_filled_bags(
+    mut commands: Commands,
+    mut replace_events: EventReader<ReplaceFilledBag>,
+    current_level: Res<CurrentLevel>,
+    game_speed: Res<GameSpeed>,
+    bag_positions: Query<&Transform, With<BagMarker>>,
+) {
+    for replaced_bag in replace_events.iter() {
+        let current_bag_position = bag_positions.get(**replaced_bag).unwrap();
         let new_bag_start = {
             let mut p = *current_bag_position;
             p.scale = Vec3::ZERO;
@@ -121,26 +209,6 @@ fn replace_full_bags(
                         &game_speed,
                     ));
             });
-
-        let exit_bag_position = {
-            let mut p = *current_bag_position;
-            p.translation.y = -(RADIUS + 0.5);
-            p
-        };
-        commands.entity(**filled_bag).insert(animations::bag_exit(
-            *current_bag_position,
-            exit_bag_position,
-            &game_speed,
-        ));
-
-        // TODO remove after https://github.com/dimforge/bevy_rapier/issues/172
-        for piece in &**bag_pieces.get(**filled_bag).unwrap() {
-            commands.entity(**filled_bag).add_child(*piece);
-
-            let bag_global = current_bag_position.translation;
-            let (piece_global, mut piece_local) = piece_positions.get_mut(*piece).unwrap();
-            piece_local.translation = piece_global.translation - bag_global;
-        }
     }
 }
 
