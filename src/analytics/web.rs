58a5d2fe486a4dd895ba5b308af4fe78629eeba9
Alex Saveau <saveau.alexandre@gmail.com>
2022-05-25 18:34:27 -0700

    Add bag representation to analytics
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/analytics/web.rs
+++ src/analytics/web.rs
@@ -1,30 +1,38 @@
-use bevy::{prelude::*, tasks::AsyncComputeTaskPool};
+use bevy::{math::const_vec3, prelude::*, tasks::AsyncComputeTaskPool};
+use bevy_rapier3d::plugin::RapierContext;
 use wasm_bindgen::prelude::*;
 
 use crate::{
+    bags::{BagMarker, BagSize},
+    colors::NominoColor,
     gb9000::GroceryBagger9000,
-    levels::{LevelFinished, LevelStarted},
-    nominos::PiecePlaced,
+    levels::{CurrentScore, LevelFinished, LevelStarted, ScoringSystems},
+    nominos::{NominoMarker, PiecePlaced, NOMINO_COLLIDER_GROUP},
 };
 
 pub struct AnalyticsPlugin;
 
 impl Plugin for AnalyticsPlugin {
     fn build(&self, app: &mut App) {
+        app.add_startup_system(init);
+
         app.add_system(log_level_start);
         app.add_system(log_level_end);
-        app.add_system(log_piece_placed);
+        app.add_system(log_piece_placed.after(ScoringSystems));
     }
 }
 
 #[derive(Copy, Clone)]
-enum ActionIds {
-    PiecePlaced = 0,
+enum VersionIds {
+    WiderRelease = 10,
 }
 
-#[derive(Copy, Clone)]
-enum VersionIds {
-    FamilyFriends = 0,
+fn init(thread_pool: Res<AsyncComputeTaskPool>) {
+    thread_pool
+        .spawn(async {
+            init_analytics(VersionIds::WiderRelease as u32);
+        })
+        .detach();
 }
 
 fn log_level_start(
@@ -63,12 +71,46 @@ fn log_level_end(
 
 fn log_piece_placed(
     mut piece_placed: EventReader<PiecePlaced>,
+    current_score: Res<CurrentScore>,
     thread_pool: Res<AsyncComputeTaskPool>,
+    rapier_context: Res<RapierContext>,
+    bags: Query<(&GlobalTransform, &BagSize), With<BagMarker>>,
+    colors: Query<&NominoColor, With<NominoMarker>>,
 ) {
-    for _ in piece_placed.iter() {
+    for PiecePlaced { bag, .. } in piece_placed.iter() {
+        let (bag_coords, bag_size) = bags.get(*bag).unwrap();
+
+        let width = bag_size.width();
+        let height = bag_size.height();
+        let block_origin = bag_coords.translation - bag_size.origin() + const_vec3!([0.5, 0.5, 0.]);
+
+        let mut bag_representation = String::with_capacity(40);
+        for row in 0..height {
+            for col in 0..width {
+                let mut color = None;
+                rapier_context.intersections_with_point(
+                    block_origin + Vec3::new(col as f32, row as f32, 0.),
+                    NOMINO_COLLIDER_GROUP.into(),
+                    None,
+                    |piece_id| {
+                        color = Some(colors.get(piece_id).unwrap());
+                        false
+                    },
+                );
+
+                if let Some(color) = color {
+                    bag_representation.push_str(&format!("{}", *color as u32 + 1));
+                } else {
+                    bag_representation.push('0');
+                }
+            }
+            bag_representation.push('\n');
+        }
+
+        let score = current_score.points;
         thread_pool
-            .spawn(async {
-                logLevelAction(ActionIds::PiecePlaced as u32);
+            .spawn(async move {
+                logPiecePlaced(score, bag_representation);
             })
             .detach();
     }
@@ -76,13 +118,11 @@ fn log_piece_placed(
 
 #[wasm_bindgen]
 extern "C" {
-    fn init(version_no: u32);
-
-    fn logLevelAction(action_id: u32);
-
-    fn logActionWithNoLevel(action_id: u32);
+    fn init_analytics(version_no: u32);
 
     fn logLevelStart(level_id: u32);
 
     fn logLevelEnd(level_id: u32);
+
+    fn logPiecePlaced(score: usize, bag_state: String);
 }
