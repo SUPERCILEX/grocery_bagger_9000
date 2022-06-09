a2e8cac552b15a3004d087b3e60aee6a1c376f54
Alex Saveau <saveau.alexandre@gmail.com>
2022-06-09 00:24:06 -0700

    Fix CI not building all crates
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- lint.sh
+++ lint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cargo fmt
-cargo clippy --fix --all-targets --all-features --allow-dirty --allow-staged --\
+cargo clippy --fix --all-targets --all-features --workspace --allow-dirty --allow-staged --\
   -W clippy::all \
   -W clippy::float_cmp_const \
   -W clippy::empty_structs_with_brackets \
